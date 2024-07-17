919f8678cfa65ebc146b00729afd70764c7f2f84
Dmitrii Belov <dmitriy.belov@legrand-ext.com>
2024-07-17 10:31:50 +0300

    mingw generator for cmake specified

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+export CC="c:/mingw64/bin/gcc"
+export CXX="c:/mingw64/bin/g++"
+
+SOURCE_DIR=$(pwd)
+echo $SOURCE_DIR
 
 # Remove the BUILD directory if it exists
 if [ -d "BUILD" ]; then
@@ -10,7 +15,7 @@ mkdir -p BUILD
 cd BUILD
 
 # Run CMake to configure the project
-cmake ..
+cmake -G "MinGW Makefiles" ..
 
 # Build the project
 cmake --build .
