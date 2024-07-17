a666e7088777bfacf2092f1d1e716bc685b51cf7
Dmitrii Belov <dmitriy.belov@legrand-ext.com>
2024-07-17 09:31:32 +0300

    - get and print git hash

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include "dummy/dummy.h"
 
+// Define a default value in case the macro is not set
+#ifndef GIT_COMMIT_HASH
+#define GIT_COMMIT_HASH "unknown"
+#endif
 
 int main()
 {
-  std::cout << func()  << std::endl;
+  std::cout << "Current Git commit hash: " << GIT_COMMIT_HASH << std::endl;
   return 0;
 }
